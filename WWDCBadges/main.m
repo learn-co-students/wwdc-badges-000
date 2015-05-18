8340798de034759299909fa3bcb571c485a9c94c
OBreaux1 <obreaux1@gmail.com>
2015-05-18 15:43:37 -0400

    Complete exercise

--- WWDCBadges/main.m
+++ WWDCBadges/main.m
@@ -14,6 +14,11 @@ int main(int argc, const char * argv[])
     @autoreleasepool {
         
         // insert code here...
+        NSArray *conferenceSpeakers = [NSArray arrayWithObjects:@"Ada",@"Edsger",@"Ada",@"Charles",@"Alan",@"Grace",@"Linus",@"Wozniak", nil];
+        
+        for (NSString *name in conferenceSpeakers) {
+            NSLog(@"Hello, my name is %@.",name);
+        }
         
     }
     return 0;
