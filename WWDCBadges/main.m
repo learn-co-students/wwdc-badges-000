1d80b2608fcf79c29d8ab7043e19d1fce48938cc
Helga108 <helgamir@gmail.com>
2015-06-01 17:27:16 +0300

    Lab done

--- WWDCBadges/main.m
+++ WWDCBadges/main.m
@@ -13,7 +13,12 @@ int main(int argc, const char * argv[])
 
     @autoreleasepool {
         
-        // insert code here...
+        NSArray *conferenceSpeakers = [[NSArray alloc] initWithObjects:@"Edsger", @"Ada", @"Charles", @"Alan", @"Grace", @"Linus", @"Wozniak", nil];
+        int i;
+        for ( i = 0; i < 7; i++ )
+        {
+            NSLog(@"Hello, my name is %@", conferenceSpeakers[i]);
+        }
         
     }
     return 0;
