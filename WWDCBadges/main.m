ff64c65c16945e7e67596d8dd50df397ca5b14f1
jamesjtong <jtong@yhmg.com>
2015-06-02 23:14:38 -0400

    loopin thru strings

--- WWDCBadges/main.m
+++ WWDCBadges/main.m
@@ -15,6 +15,11 @@ int main(int argc, const char * argv[])
         
         // insert code here...
         
+        NSArray *conferenceSpeakers = @[@"Edsger", @"Ada", @"Charles", @"Alan", @"Grace", @"Linus", @"Wozniak"];
+        for (NSString* speaker in conferenceSpeakers)
+        {
+            NSLog(@"Hello, my name is %@", speaker);
+        }
     }
     return 0;
 }
