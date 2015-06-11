0cb38cef8a2d8dfcba2187a4fda3223c4763c7d2
Crystal Chang <a.crystal.chang@gmail.com>
2015-06-11 16:33:33 -0400

    iterate over array and output

--- WWDCBadges/main.m
+++ WWDCBadges/main.m
@@ -13,7 +13,10 @@ int main(int argc, const char * argv[])
 
     @autoreleasepool {
         
-        // insert code here...
+        NSArray *conferenceSpeakers = @[@"Edsger", @"Ada", @"Charles", @"Alan", @"Grace", @"Linus", @"Wozniak"];
+        for (NSString *speaker in conferenceSpeakers){
+            NSLog(@"Hello, my name is %@.", speaker);
+        }
         
     }
     return 0;
