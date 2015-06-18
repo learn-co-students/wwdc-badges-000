f1912fca7c0853aafd318c1941f5be92e914b446
mrd333 <mr.donohue@aoiths.org>
2015-06-18 11:28:50 -0400

    for loops

--- WWDCBadges/main.m
+++ WWDCBadges/main.m
@@ -15,6 +15,14 @@ int main(int argc, const char * argv[])
         
         // insert code here...
         
+        NSArray *conferenceSpeakers = @[@"Anita Borg", @"Alan Kay", @"Ada Lovelace", @"Aaron Swartz", @"Alan Turing", @"Michael Faraday", @"Grace Hopper", @"Charles Babbage"];
+     
+        
+        for (NSInteger i=0;i<[conferenceSpeakers count];i++)
+        {
+            NSLog(@"Hello, my name is %@.", conferenceSpeakers[i]);
+        }
+        
     }
     return 0;
 }
