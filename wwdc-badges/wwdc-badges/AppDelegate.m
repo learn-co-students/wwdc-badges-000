e79a4b9768390ade2985449eb954bc760faab75c
Kyle Kukshtel <kyle.kukshtel@gmail.com>
2015-06-22 20:21:03 -0400

    Solves advanced lab objective

--- wwdc-badges/wwdc-badges/AppDelegate.m
+++ wwdc-badges/wwdc-badges/AppDelegate.m
@@ -17,17 +17,25 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
-    NSArray *conferenceSpeakers = @[@"Anita Borg",
-                                    @"Alan Kay",
-                                    @"Ada Lovelace",
-                                    @"Aaron Swartz",
-                                    @"Alan Turing",
-                                    @"Michael Faraday",
-                                    @"Grace Hopper",
-                                    @"Charles Babbage"];
+    NSArray *conferenceSpeakersFirstName = @[@"Anita",
+                                    @"Alan",
+                                    @"Ada",
+                                    @"Aaron",
+                                    @"Alan",
+                                    @"Michael",
+                                    @"Grace",
+                                    @"Charles"];
+    NSArray *conferenceSpeakersLastName = @[@"Borg",
+                                            @"Kay",
+                                            @"Lovelace",
+                                            @"Swartz",
+                                            @"Turing",
+                                            @"Faraday",
+                                            @"Hopper",
+                                            @"Babbage"];
     
-    for (NSInteger i=0; i < [conferenceSpeakers count]; i++) {
-        NSLog(@"Hello, my name is %@", conferenceSpeakers[i]);
+    for (NSInteger i=0; i < [conferenceSpeakersFirstName count]; i++) {
+        NSLog(@"Hello, my name is %@ %@", conferenceSpeakersFirstName[i], conferenceSpeakersLastName[i]);
     }
     
     return YES;
