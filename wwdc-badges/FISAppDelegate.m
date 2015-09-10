d5686c45fe3e28fbf65d8e8c910e1ec183e85fda
lresner <lyel.resner@gmail.com>
2015-09-10 15:41:17 -0400

    finished lab

--- wwdc-badges/FISAppDelegate.m
+++ wwdc-badges/FISAppDelegate.m
@@ -18,6 +18,21 @@
      
      */
     
+    NSString *Anita = @"Anita Borg";
+    NSString *Alan = @"Alan Kay";
+    NSString *Ada = @"Ada Lovelace";
+    NSString *Aaron = @"Aaron Swartz";
+    NSString *AlanT = @"Alan Turing";
+    NSString *Michael = @"Michael Faraday";
+    NSString *Grace = @"Grace Hopper";
+    NSString *Charles = @"Charles Babbage";
+    
+    NSArray *conferenceSpeakers = @[Anita, Alan, Ada, Aaron, AlanT, Michael, Grace, Charles];
+    
+    for (NSUInteger i = 0; i < [conferenceSpeakers count]; i++) {
+        NSLog(@"Hello, my name is %@", conferenceSpeakers[i]);
+    }
+    
     return YES;
 }
 
